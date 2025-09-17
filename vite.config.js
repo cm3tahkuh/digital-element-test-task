6fe531cacc17d2690b850bba97e69c66d5b92a25
cm3tahkuh <92805192+cm3tahkuh@users.noreply.github.com>
2025-09-17 20:46:42 +0500

    Реализация Header, начало реализации Hero секции (#1)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 
+
 export default defineConfig({
   // css: {
   //   preprocessorOptions: {
   //     scss: {
-  //       additionalData: `@import "src/assets/styles/_variables.scss";`
+  //       additionalData: `@use "/src/assets/styles/variables.scss";`
   //     }
   //   }
   // }
