df437b3a5ca0b35ef6bf0fa453735f5de569b81f
cm3tahkuh <markplay13498@gmail.com>
2025-09-19 22:04:53 +0500

    Доделана секция карточек, почти реализован футер

--- src/main.js
+++ src/main.js
@@ -1,27 +1,26 @@
 import "./assets/styles/main.scss";
 
-
 export default class Header {
   constructor() {
-    this.burger = document.querySelector('.header__burger');
-    this.nav = document.querySelector('.header__nav');
+    this.burger = document.querySelector(".header__burger");
+    this.nav = document.querySelector(".header__nav");
 
     this.init();
   }
 
   init() {
     if (this.burger) {
-      this.burger.addEventListener('click', () => this.toggleMenu());
+      this.burger.addEventListener("click", () => this.toggleMenu());
     }
   }
 
   toggleMenu() {
-    this.nav.classList.toggle('header__nav-active');
-    document.body.classList.toggle('no-scroll'); // опционально блокируем скролл
+    this.nav.classList.toggle("header__nav-active");
+    document.body.classList.toggle("no-scroll"); // опционально блокируем скролл
   }
 }
 
 // Инициализация
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   new Header();
 });
